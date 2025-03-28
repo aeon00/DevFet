7550d97b6bc11c678471fe5390cca8be1521d163
aeon00 <harveydienye@gmail.com>
2025-03-28 17:20:11 +0100

    debug

--- sandbox/det_band_test_2.py
+++ sandbox/det_band_test_2.py
@@ -175,7 +175,7 @@ def process_single_file(mesh_file):
 
     # Load mesh'
     start_time = time.time()
-    N = 1500
+    N = 5000
     mesh = sio.load_mesh(mesh_file)
     vertices = mesh.vertices
     num_vertices = len(vertices)
@@ -281,7 +281,7 @@ def main():
     #     sys.exit(1)
 
         # Paths
-    mesh_path = '/envau/work/meca/users/dienye.h/det_band_test_results/example_meshes/'
+    mesh_path = '/envau/work/meca/users/dienye.h/rough_hemisphere/mesh_surfaces/'
     
     print("Scanning directory: {}".format(mesh_path))
     # Get list of files
